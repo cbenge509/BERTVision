07de766178147dabafff090d3b59408c264f20b8
cbenge509 <crisbenge@hotmail.com>
2020-07-11 00:31:21 -0400

    visualizations for BERT performance

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -7,6 +7,107 @@ import numpy as np
 import h5py
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import altair as alt
+import pandas as pd
+from IPython.display import display
+
+############################################################################
+# Plotting Utilities, Constants, Methods for W209 arXiv project
+############################################################################
+
+#---------------------------------------------------------------------------
+## Plotting Palette
+#
+# Create a dict object containing U.C. Berkeley official school colors for plot palette 
+# reference : https://brand.berkeley.edu/colors/
+# secondary reference : https://alumni.berkeley.edu/brand/color-palette# CLass Initialization
+#---------------------------------------------------------------------------
+
+berkeley_palette = {'berkeley_blue'     : '#003262',
+                    'california_gold'   : '#fdb515',
+                    'founders_rock'     : '#3b7ea1',
+                    'medalist'          : '#c4820e',
+                    'bay_fog'           : '#ddd5c7',
+                    'lawrence'          : '#00b0da',
+                    'sather_gate'       : '#b9d3b6',
+                    'pacific'           : '#46535e',
+                    'soybean'           : '#859438',
+                    'south_hall'        : '#6c3302',
+                    'wellman_tile'      : '#D9661F',
+                    'rose_garden'       : '#ee1f60',
+                    'golden_gate'       : '#ed4e33',
+                    'lap_lane'          : '#00a598',
+                    'ion'               : '#cfdd45',
+                    'stone_pine'        : '#584f29',
+                    'grey'              : '#eeeeee',
+                    'web_grey'          : '#888888',
+                    # alum only colors
+                    'metallic_gold'     : '#BC9B6A',
+                    'california_purple' : '#5C3160'                   
+                    }
+
+############################################################################
+# VIZ : Define a Berkeley inspired Altair theme
+############################################################################
+
+def cal_theme():
+    font = "Lato"
+
+    return {
+        "config": {
+            "title": {
+                "fontSize": 20,
+                "font": font,
+                "anchor": "middle",
+                "align":"center",
+                "color": berkeley_palette['berkeley_blue'],
+                "subtitleFontSize": 15,
+                "subtitleFont": font,
+                "subtitleAcchor": "middle",
+                "subtitleAlign": "center",
+                "subtitleColor": berkeley_palette['pacific']
+            },
+            "axisX": {
+                "labelFont": font,
+                "titleFont": font,
+                "titleFontSize": 15,
+                "titleColor": berkeley_palette['berkeley_blue'],
+                "titleAlign": "right",
+                "titleAnchor": "end"
+            },
+            "axisY": {
+                "labelFont": font,
+                "titleFont": font,
+                "titleFontSize": 15,
+                "titleColor": berkeley_palette['berkeley_blue'],
+                "titleAlign": "right",
+                "titleAnchor": "end",
+                "gridColor": berkeley_palette['grey'],
+                "gridWidth": 1
+            },
+            "headerRow": {
+                "labelFont": font,
+                "titleFont": font,
+                "titleFontSize": 15,
+                "titleColor": berkeley_palette['berkeley_blue'],
+                "titleAlign": "right",
+                "titleAnchor": "end"
+            },
+            "legend": {
+                "labelFont": font,
+                "labelFontSize": 10,
+                "labelColor": berkeley_palette['stone_pine'],
+                "symbolType": "circle",
+                "symbolSize": 120,
+                "titleFont": font,
+                "titleFontSize": 15,
+                "titleColor": berkeley_palette['pacific']
+            }
+        }
+    }
+
+alt.themes.register("my_cal_theme", cal_theme)
+alt.themes.enable("my_cal_theme")
 
 #%%
 ############################################################################
@@ -165,3 +266,117 @@ def simple_dev_generator(labels, directory, batch_size = 32, truncate_data = Fal
             offset += 1
             output = darray
         yield output
+
+#%%
+############################################################################
+# VIZ Utilities
+#   Visualization utiltiies for training
+############################################################################
+
+def __validate_perf_df(df, expected_cols = ['epoch', 'f1', 'em']):
+    """Validates the input dataframe for type, shape, and column
+
+    Args:
+        df (Pandas.DataFrame): Dataframe to validation for input
+        expected_cols (list, optional): Which column names to verify exist in the dataframe. Defaults to ['epoch', 'f1', 'em'].
+
+    Raises:
+        ValueError: if `df` parameter is not of type Pandas.DataFrame
+        ValueError: if `df` dataframe is empty
+        ValueError: if `df` does not contain the `expected_cols` columns.
+    """
+
+    ### Input Validation
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError(f"Input parameter `df` must be of type Pandas.DataFrame. Provided value was of type '{type(df)}'.'")
+    if df.shape[0] < 1:
+        raise ValueError("Input parameter `df` must contain at least one row of data.")
+    for c in expected_cols:
+        if not c in df.columns:
+            raise ValueError(f"Input paramter `df` must contain column for '{c}' values.")
+
+    return
+
+def viz_table_scores(df, title = 'BERT-large Binary Classification on SQuAD v2', cmap = 'BuGn'):
+    """Produces a styled Pandas table output of F1 & EM performance metrics for BERTVision tasks.
+
+    Args:
+        df (Pandas.DataFrame): Pandas dataframe of the F1 and EM (exact match) metrics of of fine-tuning BERT for SQuAD v2 tasks.
+            Columns expected:
+            * epoch (float16) : ex. 0.1, 0.2, 0.3, ..., 5, 6, 7, ...
+            * f1 (float16) : ex. 0.75644543, 0.6560445, ...
+            * em (float16) : ex. 0.7843433, 0.832325, ...
+        title (str): title of the table (caption)
+        cmap (str): color map defined for pandas styler
+    """
+
+    # perform input validation
+    expected_cols = ['epoch', 'f1', 'em']
+    __validate_perf_df(df = df, expected_cols = expected_cols)
+
+
+    # limit the input dataframe to only those columns we need, and sorted by epoch
+    df = df[expected_cols].sort_values(by = 'epoch', ascending = True)
+
+    format_dict = {'epoch':'{:.1f}', 'f1': '{:.3%}', 'em': '{:.3%}'}
+
+    out = df[(df.epoch > 0)].style.set_caption(title)\
+        .format(format_dict)\
+        .background_gradient(subset=['f1', 'em'], cmap = cmap).hide_index()
+
+    display(out)
+
+    return
+
+def viz_bert_performance(df, xaxis_title = 'Training Epochs', yaxis_title = 'Performance Metric (Dev)',
+    legend_title = 'Metric', title = 'BERT-large Binary Classification Fine-Tuned Performance',
+    subtitle = 'SQuAD v2 (Answer / No Answer Detection', width = 800, height = 400):
+    """Returns an Altair visual of BERT fine-tune performance F1 / EM metrics
+
+    Args:
+        df (Pandas.DataFrame): Pandas dataframe of the F1 and EM (exact match) metrics of of fine-tuning BERT for SQuAD v2 tasks.
+            Columns expected:
+            * epoch (float16) : ex. 0.1, 0.2, 0.3, ..., 5, 6, 7, ...
+            * f1 (float16) : ex. 0.75644543, 0.6560445, ...
+            * em (float16) : ex. 0.7843433, 0.832325, ...
+        xaxis_title (str, optional): Label for the X axis. Defaults to 'Training Epochs'.
+        yaxis_title (str, optional): Label for the Y axis. Defaults to 'Performance Metric (Dev)'.
+        legend_title (str, optional): Label for the Legend. Defaults to 'Metric'.
+        title (str, optional): Title of the report. Defaults to 'BERT-large Binary Classification Fine-Tuned Performance'.
+        subtitle (str, optional): Subtitle of the report. Defaults to 'SQuAD v2 (Answer / No Answer Detection'.
+        width (int, optional): Width of the visual in pixels. Defaults to 800.
+        height (int, optional): Height of the visual in pixels. Defaults to 400.
+    """
+
+    # perform input validation
+    expected_cols = ['epoch', 'f1', 'em']
+    __validate_perf_df(df = df, expected_cols = expected_cols)
+
+    # limit the input dataframe to only those columns we need, and sorted by epoch
+    df = df[expected_cols].sort_values(by = 'epoch', ascending = True)
+
+    # transform the dataframe for visualization
+    adf = pd.melt(df, id_vars = ['epoch'], value_vars = ['f1','em'], value_name = 'score', var_name = 'metric')
+
+    min_epoch, max_epoch = min(adf.epoch), max(adf.epoch)
+    min_score, max_score = min(adf.score), max(adf.score)
+    xaxis_bins = df.epoch.values.astype(np.float16)
+    metric_range = [berkeley_palette['berkeley_blue'], berkeley_palette['lawrence']]
+
+    c = alt.Chart(adf).mark_line().encode(
+        x = alt.X('epoch:Q', axis = alt.Axis(title = xaxis_title, grid = False, 
+                formatType = "number", format = ".1", labelOverlap = True),
+            scale = alt.Scale(domain = [min_epoch - 1, max_epoch], bins = xaxis_bins)),
+        y = alt.Y('score:Q', axis = alt.Axis(title = yaxis_title, grid = True),
+            scale = alt.Scale(domain = [min_score - (.05 * min_score), max_score + (.05 * max_score)])),
+        color = alt.Color('metric',
+            scale = alt.Scale(domain = ['f1','em'], range = metric_range),
+            legend = alt.Legend(title = legend_title))
+        ).properties(width = width, height = height).configure_view(strokeWidth = 0)
+
+    display(c.properties(title = {"text": title, "subtitle": subtitle}))
+
+    return
+
+
+# %%
